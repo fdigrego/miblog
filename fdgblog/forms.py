8f02e0866098372cd4c4b2872c89ebb3dfec7f40
fdigrego <fabian.digregorio@gmail.com>
2024-08-12 21:08:50 -0300

    assign a post to the user chap 17

--- fdgblog/forms.py
+++ fdgblog/forms.py
@@ -16,7 +16,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a Title ...'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a Tag ...'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control','value': '', 'id': 'js_ref', 'type': 'hidden'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter Content ...'}),
         }
