b139c26469ea995f7c498e719a09ae416fb32011
fdigrego <fabian.digregorio@gmail.com>
2024-08-15 19:58:29 -0300

    before ch_paswd chap 25

--- members/views.py
+++ members/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     #form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
-
-def register(request):
-    return render(request, 'registration/register.html')
+    
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    #form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/edit_profile.html'
+    
+    def get_object(self):
+        return self.request.user
