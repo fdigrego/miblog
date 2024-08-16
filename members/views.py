b40e20bfec8fc4439dc0d8b20c2bc55d52983c0d
fdigrego <fabian.digregorio@gmail.com>
2024-08-15 21:18:34 -0300

    change password chap 25

--- members/views.py
+++ members/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    success_url = reverse_lazy('password_success')
+    
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
-    #form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
     
 class UserEditView(generic.UpdateView):
     form_class = EditProfileForm
-    #form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/edit_profile.html'
     
